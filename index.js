a2f724467ce542dfa2b9b04043f593d58ecf16d2
Che Mok <chemok78@gmail.com>
2017-05-11 04:34:32 +0000

    getting user data in a team using slack.com/api.users.list

--- index.js
+++ index.js
@@ -17,19 +17,16 @@ const server = app.listen(process.env.PORT || 8080, () => {
 app.post('/', (req, res) => {
   // implement your bot here ...
   let text = req.body.text;
-  
   let data = {
     response_type: 'in_channel', //public to the channel
     text: 'test',
     attachments:[
       {
-        image_url: ''
+        image_url: 'http://cdn2.sbnation.com/imported_assets/1974127/BaS_2_ECEAAWcA_.jpg'
       }  
     ]
   };
-  
   res.json(data);
-  
 });
 
 app.get('/slack/oauth', (req, res) => {
@@ -46,20 +43,35 @@ app.get('/slack/oauth', (req, res) => {
   };
 
   request.post('https://slack.com/api/oauth.access', data, (error, response, body) => {
+  //get authorisation token first  
     if (!error && response.statusCode == 200) {
       // You are done.
       // If you want to get team info, you need to get the token here
       let token = JSON.parse(body).access_token; // Auth token
 
+      //get the team info and redirect to the team app management page
       request.post('https://slack.com/api/team.info', {form: {token: token}}, (error, response, body) => {
         if (!error && response.statusCode == 200) {
           if(JSON.parse(body).error == 'missing_scope') {
-            res.send('Gif has been added to your team!');
+            res.send('Calendar Bot has been added to your team!');
           } else {
             let team = JSON.parse(body).team.domain;
             res.redirect(`http://${team}.slack.com/apps/manage`);
           }
         }
+      });
+      
+      //get team list info from Slack API users.list method
+      //for now this request is in the authorisation part
+      request.post('https://slack.com/api/users.list', {form: {token: token}}, (error, response, body) => {
+      /*Apps created after January 4th, 2017 must request both the users:read and users:read.email 
+      OAuth permission scopes when using the OAuth app installation flow to enable access to the email field
+      of user objects returned by this method.*/    
+          let membersList = JSON.parse(body);
+          membersList.members.forEach((member)=>{
+          //the emailaddress is in the profile object for every user
+            console.log(member.profile.email);
+          });
       });
     }
   });
